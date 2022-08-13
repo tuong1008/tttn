e42903abfe5e72191f6923d6c3f17ee752a7ee9e
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2022-08-13 22:36:46 +0700

    javascript

--- src/main/webapp/WEB-INF/resource/javascript/slide.js
+++ src/main/webapp/WEB-INF/resource/javascript/slide.js
@@ -1,27 +1,30 @@
-var btnNext = document.getElementById('btn__slide--next');
-var slideShowNext = function () {
-    var slideList = document.getElementsByClassName('slide__item');
-    var n = slideList.length;
-    for (var i = 0; i < n; i++) {
+const btnNext = document.getElementById('btn__slide--next');
+const slideShowNext = function () {
+    let slideList = document.getElementsByClassName('slide__item');
+    let n = slideList.length;
+    for (let i = 0; i < n; i++) {
         if (slideList[i].classList.contains('slide__item--active')) {
             slideList[i].classList.remove('slide__item--active');
-            var idx = (i == n - 1) ? 0 : i + 1;
+            let idx = (i === n - 1) ? 0 : i + 1;
             slideList[idx].classList.add('slide__item--active');
             break;
         }
     }
-}
+};
+
 btnNext.onclick = function () {
     slideShowNext();
 }
-var btnPrev = document.getElementById('btn__slide--prev');
+
+let btnPrev = document.getElementById('btn__slide--prev');
+
 btnPrev.onclick = function () {
-    var slideList = document.getElementsByClassName('slide__item');
-    var n = slideList.length;
-    for (var i = 0; i < n; i++) {
+    let slideList = document.getElementsByClassName('slide__item');
+    let n = slideList.length;
+    for (let i = 0; i < n; i++) {
         if (slideList[i].classList.contains('slide__item--active')) {
             slideList[i].classList.remove('slide__item--active');
-            var idx = (i == 0) ? n - 1 : i - 1;
+            let idx = (i === 0) ? n - 1 : i - 1;
             slideList[idx].classList.add('slide__item--active');
             break;
         }
@@ -29,5 +32,5 @@ btnPrev.onclick = function () {
 }
 setInterval(function () {
     slideShowNext();
-}, 3000)
+}, 2000);
 
